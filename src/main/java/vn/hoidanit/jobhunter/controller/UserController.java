58fdf6b2bda2605be20f1f6b36071c365eb54e17
VanDung <3ngk1sha@gmail.com>
2024-05-13 13:43:45 +0700

    test CRUD API

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,12 +1,48 @@
 package vn.hoidanit.jobhunter.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.service.UserService;
+
+import java.util.List;
 
 @RestController
 public class UserController {
-    @GetMapping("/user/create")
-    public String createNewUser() {
-        return "12312";
+    @Autowired
+    UserService userService;
+
+    //@GetMapping("/user/create")
+    @PostMapping("/user/create")
+    public User createNewUser( @RequestBody User user ) {
+//        User newUser = new User();
+//        newUser.setEmail("dungbn765@gmail.com");
+//        newUser.setName("Dungbn765");
+//        newUser.setPassword("123");
+       User newUser = this.userService.SaveUser(user);
+        return newUser;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public void deleteUser( @PathVariable long id) {
+        this.userService.DeleteUserById(id);
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUserbyId( @PathVariable("id") long id ) {
+       return this.userService.GetUserById(id);
+
+    }
+
+    @GetMapping("/user")
+    public List<User> getAllUsers() {
+        return this.userService.GetAllUsers();
+
+    }
+
+    @PutMapping("/user")
+    public User updateUser( @RequestBody User user ) {
+
+        return this.userService.UpdateUser(user);
     }
 }
---
+++ src/main/java/vn/hoidanit/jobhunter/service/UserService.java
@@ -0,0 +1,41 @@
+package vn.hoidanit.jobhunter.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+    public User SaveUser(User user){
+       return this.userRepository.save(user);
+    }
+
+    public void DeleteUserById(long id){
+        this.userRepository.deleteById(id);
+    }
+
+    public User GetUserById(long id){
+        Optional<User> user = this.userRepository.findById(id);
+        if(user.isPresent()){
+            return user.get();
+        }
+        else return null;
+    }
+    public List<User> GetAllUsers(){
+        return this.userRepository.findAll();
+    }
+
+    public User UpdateUser(User newuser){
+       Optional<User> user = this.userRepository.findById(newuser.getId());
+       if(user.isPresent()){
+           this.userRepository.save(newuser);
+           return newuser;
+       }else return null;
+    }
+}
